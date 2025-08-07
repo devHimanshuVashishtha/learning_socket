c3c200a4039bc075ad8dd553f0333b0853f76f16
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-07 12:53:11 +0530

    created the Schema for the message storing

---
+++ models/messagemodel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("Message", messageSchema);
