9195ba42442b9e1cf040f222fdee0dfe09b6352a
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-07 12:48:09 +0530

    create the database and connect it with the server

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
+const connectDB = require("./config/database");
 require("dotenv").config();
+
+connectDB();
 
 const app = express();
 const server = createServer(app);
