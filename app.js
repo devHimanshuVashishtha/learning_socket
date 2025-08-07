adc441a07a04b337ec1b417aa7a2f1634c7dd510
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-07 17:38:12 +0530

    added th tenMinute feature and do some error handling in the client side

--- app.js
+++ app.js
@@ -32,8 +32,11 @@ io.on("connection", async (socket) => {
       "session detected: fetching message history from the database."
     );
     try {
-      const message = await Message.find().sort({ createdAt: -1 }).limit(20);
-      message.reverse().forEach((mess) => {
+      const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
+      const messages = await Message.find({
+        createdAt: { $gte: tenMinutesAgo },
+      }).sort({ createdAt: 1 });
+      messages.forEach((mess) => {
         socket.emit(
           "chat msg",
           { content: mess.content, username: mess.username },
@@ -42,6 +45,7 @@ io.on("connection", async (socket) => {
       });
     } catch (err) {
       console.error("ERROR:", err);
+      socket.emit('error message', 'previous messages can not be loaded.');
     }
   }
 
@@ -59,6 +63,7 @@ io.on("connection", async (socket) => {
         );
       } else {
         console.log("Enter a valid message");
+        socket.emit('error message', 'Your message not sent.');
       }
     } catch (err) {
       console.err("Error:", err);
