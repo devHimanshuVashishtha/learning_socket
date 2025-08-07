6f97cff69b28aea5ea7d1265c1864387df282357
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-07 11:44:13 +0530

    improve folder structure and add bsic validation and basic error handling

--- app.js
+++ app.js
@@ -2,25 +2,40 @@ const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
 const { Server } = require("socket.io");
+require("dotenv").config();
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
+app.use(express.static(join(__dirname, "./public/index.html")));
+
 app.get("/", (req, res) => {
-  res.sendFile(join(__dirname, "index.html"));
+  res.sendFile(join(__dirname, "public", "index.html"));
 });
+server.on("error", (err) => {
+  console.Error("Server Error:", err);
+});
+
 io.on("connection", (socket) => {
   console.log("a user connected");
   socket.on("chat msg", (msg) => {
-    console.log("Message:", msg);
-    io.emit("chat msg", msg);
+    if (msg && msg.trim().length > 0) {
+      console.log("Message:", msg);
+      io.emit("chat msg", msg);
+    } else {
+      console.log("Enter a valid message");
+    }
   });
-  socket.on("disconnect", () => {
-    console.log("a user disconnected");
+  socket.on("disconnect", (resa) => {
+    console.log("a user disconnected", resa);
+  });
+
+  socket.on("error", (err) => {
+    console.error("socket error:", err);
   });
 });
 
-server.listen(3000, () => {
-  console.log(`server started at http://localhost:3000`);
+server.listen(process.env.PORT, () => {
+  console.log(`server started at http://localhost:${process.env.PORT}`);
 });
