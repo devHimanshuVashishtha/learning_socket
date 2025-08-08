37c894b4d3c34447c3fe676e811777985fb7564b
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-08-08 11:47:28 +0530

    add the clientOffset for acknowledgement form client side and do some more upgradation for the serverOff for restore the message

--- app.js
+++ app.js
@@ -26,9 +26,29 @@ server.on("error", (err) => {
 io.on("connection", async (socket) => {
   socket.username = `User-${socket.id.substring(0, 5)}`;
   console.log("a user connected", socket.username);
-  if (!socket.recovered) {
+  if (socket.handshake.auth.serverOffset) {
+    console.log(`recovering message for ${socket.username}`);
+    try {
+      const messages = await Message.find({
+        _id: { $gt: socket.handshake.auth.serverOffset },
+      }).sort({ createdAt: 1 });
+      messages.forEach((msg) => {
+        socket.emit(
+          "chat msg",
+          {
+            content: msg.content,
+            username: msg.username,
+          },
+          msg._id.toString()
+        );
+      });
+    } catch (err) {
+      console.error("Error:", err);
+      socket.emit("error message", "Failed to recover your previous data");
+    }
+  } else if (!socket.recovered) {
     console.log(
-      "session detected: fetching message history from the database."
+      "New session detected: fetching message history from the database."
     );
     try {
       const tenMinutesAgo = new Date(Date.now() - 10 * 60 * 1000);
@@ -39,33 +59,40 @@ io.on("connection", async (socket) => {
         socket.emit(
           "chat msg",
           { content: mess.content, username: mess.username },
-          mess._id
+          mess._id.toString()
         );
       });
     } catch (err) {
       console.error("ERROR:", err);
-      socket.emit('error message', 'previous messages can not be loaded.');
+      socket.emit("error message", "previous messages can not be loaded.");
     }
   }
 
-  socket.on("chat msg", async (msg) => {
-    const newMessage = new Message({ content: msg, username: socket.username });
+  socket.on("chat msg", async (msg, clientOffset, callback) => {
+    const newMessage = new Message({
+      content: msg,
+      username: socket.username,
+      clientOffset: clientOffset,
+    });
     try {
       if (msg && msg.trim().length > 0) {
         console.log("Message:", msg);
 
         await newMessage.save();
+        const serverOffset = newMessage._id.toString();
         io.emit(
           "chat msg",
           { content: newMessage.content, username: newMessage.username },
-          newMessage._id
+          serverOffset
         );
+        callback(serverOffset);
       } else {
         console.log("Enter a valid message");
-        socket.emit('error message', 'Your message not sent.');
+        socket.emit("error message", "Your message not sent.");
+        callback(null, "failed to return call back");
       }
     } catch (err) {
-      console.err("Error:", err);
+      console.error("Error:", err);
     }
   });
   socket.on("disconnect", (resa) => {
